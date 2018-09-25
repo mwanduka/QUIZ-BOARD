85d1c3601b1c9628534c2642672a4b5c7cfa3886
mwanduka <42966698+mwanduka@users.noreply.github.com>
2018-09-25 07:21:31 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-var answer = $("input:radio[name=answer]:checked").val();
+$(document).ready(function(){
+  $("form#Quiz").submit(function(event){
+    var question1= $("input:radio[name=question1]:checked").val();
+    var question2= $("input:radio[name=question2]:checked").val();
+    var question3= $("input:radio[name=question3]:checked").val();
+
+    var result=parseInt(question1)+parseInt(question2)+parseInt(question3);
+    $("#result").text("Your scored: " +result + " %");
+
+    $("button").click(function() {
+      $("#result-showing").toggle();
+      $("#Quiz-hidden").toggle();
+    });
+      $("form#Quiz").hide();
+      $("#result").show();
+
+      event.preventDefault();
+    });
+});
